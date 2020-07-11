69985de5b8d4e9146d24126df9e7b6864085142e
LASER-Yi <ly0007@yeah.net>
2020-07-11 10:48:41 +0800

    init repo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager
+{
+    private static GameManager _instance;
+    public static GameManager Instance 
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = new GameManager();
+            }
+
+            return _instance;
+        }
+    }
+
+    
+}
